c03be3d14434a6a620a15f2b8e152668be75e5cf
git-coder257 <85864939+git-coder257@users.noreply.github.com>
2022-05-01 20:06:52 +0100

    createaccount feature

--- src/components/createaccount.tsx
+++ src/components/createaccount.tsx
@@ -14,14 +14,23 @@ interface geolocation {
     longitude?: number
 }
 
+interface postoffice {
+    user_id: number,
+    username?: string,
+    password?: string,
+    postofficename: string
+}
+
 const Createaccount: React.FC = () => {
 
-    let [username, setusername] = useState("")
-    let [password, setpassword] = useState("")
-    let [location, setlocation] = useState("")
-    let [province, setprovince] = useState("")
-    let [postofficename, setpostofficename] = useState("")
+    let [username, setusername] = useState<string>("")
+    let [password, setpassword] = useState<string>("")
+    let [location, setlocation] = useState<string>("")
+    let [district, setdistrict] = useState<string>("")
+    let [postofficename, setpostofficename] = useState<string>("")
     let [geolocation, setgeolocation] = useState<geolocation>({})
+    let [error, seterror] = useState("")
+    let [postoffices, setpostoffices] = useState<postoffice[]>([])
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -37,32 +46,59 @@ const Createaccount: React.FC = () => {
         setpassword(val.target.value)
     }
 
-    const handleupdatepostofficename = (val: val) => {
-        setpostofficename(val.target.value)
-    }
+    // const handleupdatepostofficename = (val: val) => {
+    //     setpostofficename(val.target.value)
+    // }
 
     const handleupdatelocation = (val: val) => {
         setlocation(val.target.value)
     }
 
     const handleupdateprovince = (val: val) => {
-        setprovince(val.target.value)
+        setdistrict(val.target.value)
     }
 
     const handlecreateaccount = async () => {
-        // console.log()
-        axios.get(`https://api.myptv.com/geocoding/v1/locations/by-text?searchText=${location}&apiKey=ZjVmOTA2NmQ5YTZkNDk0NWEzNDQzNTIxOWY0MTdlODc6ZTA5YzNjY2QtN2FjOC00NTA5LWE2NjgtYjA1NGE4Nzg1NGY1`)
+        // axios.get(`https://api.myptv.com/geocoding/v1/locations/by-text?searchText=${`${location} ${district}`}&apiKey=ZjVmOTA2NmQ5YTZkNDk0NWEzNDQzNTIxOWY0MTdlODc6ZTA5YzNjY2QtN2FjOC00NTA5LWE2NjgtYjA1NGE4Nzg1NGY1`)
+        //     .then((r) => {
+        //         console.log(r.data)
+        //     })
+
+        axios.get(`http://localhost:3000/postofficename/${district}`)
             .then((r) => {
-                console.log(r.data)
+                // console.log(r.data)
+                if (r.data.success){
+
+                    let response: postoffice[] = r.data.postoffices
+
+                    for (let i = 0; i < response.length; i++){
+                        delete response[i].password
+                        delete response[i].username
+                    }
+
+                    setpostoffices(response)
+                }
             })
+        
+        
+    }
+
+    const handleselectpostoffice = (index: number) => {
+        setpostofficename(postoffices[index].postofficename)
     }
 
     return <div>
         <input placeholder="username" onChange={handleupdateusername}/><br/>
         <input placeholder="password" onChange={handleupdatepassword}/><br/>
-        <input placeholder="post office name" onChange={handleupdatepostofficename}/><br/>
+        {/* <input placeholder="post office name" onChange={handleupdatepostofficename}/><br/> */}
         <input placeholder="address" onChange={handleupdatelocation}/><br/>
-        <input placeholder="province" onChange={handleupdateprovince}/><br/>
+        <input placeholder="district" onChange={handleupdateprovince}/><br/>
+        {postoffices.length > 0 && <span>selecct post office</span>}
+        {postoffices.map((postoffice, index: number) => <div key={index}>
+            <button onClick={() => handleselectpostoffice(index)}>
+                {postoffice.postofficename}
+            </button>
+        </div>)}
         <button onClick={handlecreateaccount}>Create account</button>
     </div>
 }
